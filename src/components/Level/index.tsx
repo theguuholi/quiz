1729d0b751cbd96cfb862595e1d34029363bdaa1
Gus <g.92oliveira@gmail.com>
2025-01-28 13:48:24 -0300

    add more animated text

--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -1,9 +1,10 @@
-import { Easing, Pressable, PressableProps,Text, View } from 'react-native';
+import { Easing, Pressable, PressableProps, Text, View } from 'react-native';
 
 import { THEME } from '../../styles/theme';
 import { styles } from './styles';
-import Animated, { useAnimatedStyle, useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
+import Animated, { interpolateColor, useAnimatedStyle, useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
 import { AnimatedView } from 'react-native-reanimated/lib/typescript/component/View';
+import { useEffect } from 'react';
 
 const TYPE_COLORS = {
   EASY: THEME.COLORS.BRAND_LIGHT,
@@ -19,20 +20,45 @@ type Props = PressableProps & {
 
 export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Props) {
   const scale = useSharedValue(1);
+  const checked = useSharedValue(0);
   const animatedcontainerStyle = useAnimatedStyle(() => {
     return {
-      transform: [{ scale: scale.value }]
-  }})
+      transform: [{ scale: scale.value }],
+      backgroundColor: interpolateColor(
+        checked.value,
+        [0, 1],
+        ["transparent", COLOR]
+      )
+    }
+  })
+
+  const animatedTextStyle = useAnimatedStyle(() => {
+    return {
+      color: interpolateColor(
+        checked.value,
+        [0, 1],
+        [COLOR, THEME.COLORS.GREY_100]
+      )
+    }
+  })
 
   const COLOR = TYPE_COLORS[type];
 
   const onPressIn = () => {
-    scale.value = withSpring(1.1, {duration: 200})
+    scale.value = withSpring(1.1, { duration: 200 })
     // scale.value = withSpring(1.5, {easing: Easing.bounce})
   }
   const onPressOut = () => {
     scale.value = withTiming(1)
   }
+
+  useEffect(() => {
+    checked.value = withTiming(isChecked ? 1 : 0, {
+      duration: 200,
+      easing: Easing.ease
+    })
+
+  }, [isChecked])
 
 
   return (
@@ -44,16 +70,15 @@ export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Prop
         [
           styles.container,
           animatedcontainerStyle,
-          { borderColor: COLOR, backgroundColor: isChecked ? COLOR : 'transparent' }
+          { borderColor: COLOR }
         ]
       }>
-        <Text style={
+        <Animated.Text style={
           [
             styles.title,
-            { color: isChecked ? THEME.COLORS.GREY_100 : COLOR }
-          ]}>
+            animatedTextStyle]}>
           {title}
-        </Text>
+        </Animated.Text>
       </Animated.View>
     </Pressable>
   );
