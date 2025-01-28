b6842ae83b695ad09d8c067bb43bffa927edec68
Gus <g.92oliveira@gmail.com>
2025-01-28 12:25:19 -0300

    first time

--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -1,7 +1,9 @@
-import { TouchableOpacity, TouchableOpacityProps, Text, View } from 'react-native';
+import { Easing, Pressable, PressableProps,Text, View } from 'react-native';
 
 import { THEME } from '../../styles/theme';
 import { styles } from './styles';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
+import { AnimatedView } from 'react-native-reanimated/lib/typescript/component/View';
 
 const TYPE_COLORS = {
   EASY: THEME.COLORS.BRAND_LIGHT,
@@ -9,21 +11,39 @@ const TYPE_COLORS = {
   MEDIUM: THEME.COLORS.WARNING_LIGHT,
 }
 
-type Props = TouchableOpacityProps & {
+type Props = PressableProps & {
   title: string;
   isChecked?: boolean;
   type?: keyof typeof TYPE_COLORS;
 }
 
 export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Props) {
+  const scale = useSharedValue(1);
+  const animatedcontainerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scale.value }]
+  }})
 
   const COLOR = TYPE_COLORS[type];
 
+  const onPressIn = () => {
+    scale.value = withSpring(1.1, {duration: 200})
+    // scale.value = withSpring(1.5, {easing: Easing.bounce})
+  }
+  const onPressOut = () => {
+    scale.value = withTiming(1)
+  }
+
+
   return (
-    <TouchableOpacity {...rest}>
-      <View style={
+    <Pressable {...rest}
+      onPressIn={onPressIn}
+      onPressOut={onPressOut}
+    >
+      <Animated.View style={
         [
           styles.container,
+          animatedcontainerStyle,
           { borderColor: COLOR, backgroundColor: isChecked ? COLOR : 'transparent' }
         ]
       }>
@@ -34,7 +54,7 @@ export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Prop
           ]}>
           {title}
         </Text>
-      </View>
-    </TouchableOpacity>
+      </Animated.View>
+    </Pressable>
   );
 }
